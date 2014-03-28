fa930adff3525a75602c7f6ca5bfd611f60fb424
JasonBristol <gethsemane369@gmail.com>
2014-03-28 17:57:32 -0400

    Started work on BNE

--- mips-simple.v
+++ mips-simple.v
@@ -275,6 +275,21 @@ module MainControl (Op,Control);
 endmodule
 
 
+module BranchControl (BranchOp,Zero,BranchOut);
+
+  input [1:0] BranchOp;
+  input Zero;
+  output BranchOut;
+  wire ZeroInvert,i0,i1;
+
+  not not1(ZeroInvert,Zero);
+  and and1(i0,BranchOp[0],Zero);
+  and and2(i1,BranchOp[1],ZeroInvert);
+  or or1(BranchOut,i0,i1);
+
+endmodule
+
+
 module CPU (clock,WD,IR);
 
   input clock;
