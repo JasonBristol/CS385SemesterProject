654a3a60660656aac3871800bfbaba4f7bf8fe8e
JasonBristol <gethsemane369@gmail.com>
2014-04-10 11:26:09 -0400

    Commented out BranchControl

--- mips-pipe3.v
+++ mips-pipe3.v
@@ -275,19 +275,19 @@ module MainControl (Op,Control);
 endmodule
 
 
-module BranchControl (BranchOp,Zero,BranchOut);
+// module BranchControl (BranchOp,Zero,BranchOut);
 
-  input [1:0] BranchOp;
-  input Zero;
-  output BranchOut;
-  wire ZeroInvert,i0,i1;
+//   input [1:0] BranchOp;
+//   input Zero;
+//   output BranchOut;
+//   wire ZeroInvert,i0,i1;
 
-  not not1(ZeroInvert,Zero);
-  and and1(i0,BranchOp[0],Zero);
-  and and2(i1,BranchOp[1],ZeroInvert);
-  or or1(BranchOut,i0,i1);
+//   not not1(ZeroInvert,Zero);
+//   and and1(i0,BranchOp[0],Zero);
+//   and and2(i1,BranchOp[1],ZeroInvert);
+//   or or1(BranchOut,i0,i1);
 
-endmodule
+// endmodule
 
 module CPU (clock,PC,IFID_IR,IDEX_IR,WD);
 
